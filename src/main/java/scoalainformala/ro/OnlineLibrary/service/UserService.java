4d4df00ae9e49007307063022ab37592881f0546
Sorin <56653432+sorinbobocel@users.noreply.github.com>
2020-07-23 15:08:13 +0300

    Third commit: service and repo layers

--- src/main/java/scoalainformala/ro/OnlineLibrary/service/UserService.java
+++ src/main/java/scoalainformala/ro/OnlineLibrary/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import scoalainformala.ro.OnlineLibrary.domain.LibraryUser;
 import scoalainformala.ro.OnlineLibrary.dto.UserEditDto;
 import scoalainformala.ro.OnlineLibrary.dto.UserInsertDto;
+import scoalainformala.ro.OnlineLibrary.exceptions.InvalidUserException;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,15 +12,13 @@ import java.util.UUID;
 @Service
 public interface UserService {
 
-    public List<LibraryUser> findAll();
-
     public LibraryUser findByEmail(String email);
 
     public UserEditDto showUser(String email);
 
     public LibraryUser saveNewUser(UserInsertDto userInsertDto);
 
-    public void saveExistingUser(UserEditDto userEditDto);
+    public UserEditDto saveUserEdit(UserEditDto userEditDto);
 
     public void deleteById(UUID id);
 }
